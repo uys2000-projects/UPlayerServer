f4a8787144bd9992dea3f1568c586c63419b95cc
Mehmet Uysal <mehmetuysal2000@yandex.com>
2024-04-21 12:06:18 +0300

    test

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -4,7 +4,10 @@ export const getFile = function (url: string) {
   return new Promise((resolve, reject) => {
     const writeMessage = (message: http.IncomingMessage) => {
       let data: any[] = [];
-      message.on("data", (chunk) => data.push(chunk));
+      message.on("data", (chunk) => {
+        console.log(chunk.toString());
+        data.push(chunk);
+      });
       message.on("end", () => {
         console.log(`Data read endded data length:${data.length}`);
         resolve(Buffer.concat(data).toString());
